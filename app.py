58fa7f4089a27e174daa01c3b3a4e61316ae8b32
peymanbayat <53484770+peymanbayat@users.noreply.github.com>
2023-10-07 19:01:16 +0330

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ if app_mode == 'Run on Video':
     #Input for Video
     video_file = st.sidebar.file_uploader("Upload a Video",type=['mp4','mov','avi','asf','m4v'])
     st.sidebar.markdown("---")
-    tffile = tempfile.NamedTemporaryFile(delete=False)
+    #tffile = tempfile.NamedTemporaryFile(delete=False)
     
     if not video_file:
         vid = cv2.VideoCapture(demo_video)
